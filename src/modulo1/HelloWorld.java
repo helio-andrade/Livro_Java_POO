b90ca04795eca816e0737b6b10b08cd86a341cef
Hélio Andrade <helio_andrade@hotmail.com.br>
2022-12-27 16:03:19 -0300

    Commit initial

---
+++ src/modulo1/HelloWorld.java
@@ -0,0 +1,12 @@
+package modulo1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Parâmetro 01: " + args[0]);
+		System.out.println("Parâmetro 02: " + args[1]);
+		System.out.println("Hello, World");
+
+	}
+
+}
