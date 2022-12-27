eab7e74e874f3f49a149425fd93127b443eeea54
Hélio Andrade <helio_andrade@hotmail.com.br>
2022-12-27 19:56:13 -0300

    Update ExemploMetodo.java

--- src/modulo1/ExemploMetodo.java
+++ src/modulo1/ExemploMetodo.java
@@ -1,11 +1,20 @@
 package modulo1;
 
 // Exemplo de métodos, variáveis e atributos
+import	java.util.Date;
+
 public class ExemploMetodo {
-
+	private int meuAtributo = 0; // pode ser usada por qualquer método
+	
 	public static void main(String[] args) {
-		
-
+		Date today = new Date();
+		System.out.println(today);
+		imprimir();
 	}
 
+	private static void imprimir() {
+		// esta variável poderá ser usada somente no método imprimir()
+		int minhaVariavel = 0;
+		System.out.println("método imprimir");
+	}
 }
