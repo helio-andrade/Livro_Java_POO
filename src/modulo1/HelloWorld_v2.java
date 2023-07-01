1d57fc6b6edaf8e4c24d2b87d4b18965416c3a5e
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-07-01 16:36:41 -0300

    Update

--- src/modulo1/HelloWorld_v2.java
+++ src/modulo1/HelloWorld_v2.java
@@ -1,12 +1,13 @@
 // Programa 01.01
 package modulo1;
 
-import static java.lang.System.*;
+import static java.lang.System.out;
 
-	// Imprime na tela os argumentos recebidos e a frase "Hello, World"
+// Imprime na tela os argumentos recebidos e a frase "Hello, World"
 public class HelloWorld_v2 {
 
 	public static void main(String[] args) {
+		
 		out.println("Parâmetro 01: " + args[0]);
 		out.println("Parâmetro 02: " + args[1]);
 		out.println("Hello, World");
--- src/modulo1/Principal.java
+++ src/modulo1/Principal.java
@@ -1,6 +1,8 @@
 // Programa 01.03
 package modulo1;
 
+import static java.lang.System.out;
+
 // Criação de objetos em Java
 public class Principal {
 
@@ -8,6 +10,18 @@ public class Principal {
 		ContaCorrente objeto1 = new ContaCorrente(); // Criando o objeto 1
 		ContaCorrente objeto2 = new ContaCorrente(); // Criando o objeto 2
 		
+		objeto1.setSaldo(1234.56);
+		out.println("saldo1 = R$ " + objeto1.getSaldo());
+		
+		objeto2.setAgencia(1234);
+		objeto2.setConta(4567);
+		objeto2.setSaldo(9874.56);
+		objeto2.efetuarSaque(874.56);
+		
+		out.println("\nobjeto2:" +
+					"\nAg: "		 + objeto2.getAgencia() + 
+					"\nC/c: "      + objeto2.getConta() +
+					"\nSaldo: R$ " + objeto2.getSaldo());
 	}
 
 }
