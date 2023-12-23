9a62293f0ef101281191e305a43bb16fc18c8671
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-12-23 15:52:52 -0300

    Create ExemploIfElse.java

---
+++ src/modulo1/ExemploIfElse.java
@@ -0,0 +1,20 @@
+package modulo1;
+
+// Exemplo dos comandos if e else
+public class ExemploIfElse {
+	public static void main(String[] args) {
+		int var1 = 10;
+		int var2 = 20;
+		
+		// modo de uso: if (condição)
+		if (var1 > var2) {
+			// bloco de comandos do if
+			System.out.println("var1 é maior que var2");
+		}
+		else {
+			// bloco de comandos else
+			System.out.println("var1 é menor que var2");
+		}
+				
+	}
+}
